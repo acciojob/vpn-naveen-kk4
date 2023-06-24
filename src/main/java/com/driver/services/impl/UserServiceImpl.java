bd7a2d01d47d3de5d1b840f3fd7bbb8294a89655
naveen-kk4 <naveennig2001@gmail.com>
2023-06-25 01:08:43 +0530

    first

--- src/main/java/com/driver/services/impl/UserServiceImpl.java
+++ src/main/java/com/driver/services/impl/UserServiceImpl.java
@@ -60,12 +60,8 @@ public class UserServiceImpl implements UserService {
     @Override
     public User subscribe(Integer userId, Integer serviceProviderId) {
         User user = userRepository3.findById(userId).get();
-        Country country = user.getOriginalCountry();
         ServiceProvider serviceProvider = serviceProviderRepository3.findById(serviceProviderId).get();
 
-        serviceProvider.getCountryList().add(country);
-        country.setServiceProvider(serviceProvider);
-        serviceProviderRepository3.save(serviceProvider);
         user.getServiceProviderList().add(serviceProvider);
         serviceProvider.getUsers().add(user);
 
